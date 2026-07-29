9a683105e17ae149e9c08363a963c95e96bc6070
asaromi <a.sabililromi@gmail.com>
2026-07-29 20:24:49 +0700

    api: trial update to trigger github action

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,7 +5,7 @@ import reviewRouter from './reviews/reviews.controller'
 import { ApiResponse } from './common/common.dto'
 
 const app = new Hono<Env>()
-app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:3000', 'https://asaromi.pages.dev'] }))
+app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:3000', 'https://asaromi.pages.dev', 'https://asaromi.biz.id'] }))
 app.get('/', (c) => c.json(new ApiResponse("Hello, World!")))
 
 // list of routes
