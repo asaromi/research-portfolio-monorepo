1b7fc573ac8a5f6bce1937eca63fdf7c84652d09
asaromi <a.sabililromi@gmail.com>
2026-07-29 20:19:02 +0700

    trial auto build workers using github action

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,10 +2,11 @@ import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import { Env } from './common/type'
 import reviewRouter from './reviews/reviews.controller'
+import { ApiResponse } from './common/common.dto'
 
 const app = new Hono<Env>()
 app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:3000', 'https://asaromi.pages.dev'] }))
-app.get('/', (c) => c.text('Hello Hono!'))
+app.get('/', (c) => c.json(new ApiResponse("Hello, World!")))
 
 // list of routes
 app.route('/reviews', reviewRouter)
