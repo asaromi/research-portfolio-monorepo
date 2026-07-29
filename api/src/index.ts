a4f28e10e833a9cf071f673e154a6ff6d2602f7f
asaromi <a.sabililromi@gmail.com>
2026-07-29 20:32:13 +0700

    build: add yarn.lock to trigger github action

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,8 +5,8 @@ import reviewRouter from './reviews/reviews.controller'
 import { ApiResponse } from './common/common.dto'
 
 const app = new Hono<Env>()
-app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:3000', 'https://asaromi.pages.dev', 'https://asaromi.biz.id'] }))
-app.get('/', (c) => c.json(new ApiResponse("Hello, World!")))
+app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:3000', 'https://asaromi.pages.dev'] }))
+app.get('/', (c) => c.json(new ApiResponse('Hello, World!')))
 
 // list of routes
 app.route('/reviews', reviewRouter)
