dc244392ca05fedbc58fd72ffdc11c4e06475d08
asaromi <a.sabililromi@gmail.com>
2026-07-30 20:35:43 +0700

    build: trial to use routing asaromi.pages.dev/api

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,7 +4,7 @@ import { Env } from './common/type'
 import reviewRouter from './reviews/reviews.controller'
 import { ApiResponse } from './common/common.dto'
 
-const app = new Hono<Env>()
+const app = new Hono<Env>().basePath('/api')
 app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:3000', 'https://asaromi.pages.dev', 'https://asaromi.biz.id'] }))
 app.get('/', (c) => c.json(new ApiResponse('Hello, World!')))
 
